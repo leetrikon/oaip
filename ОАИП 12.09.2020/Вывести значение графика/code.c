f5e85a498294bda7edc1a869232772dd62704bee
leetrikon <49814202+leetrikon@users.noreply.github.com>
2020-09-14 10:25:29 +0700

    Update code.c

--- ОАИП 12.09.2020/Вывести значение графика/code.c
+++ ОАИП 12.09.2020/Вывести значение графика/code.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
-// Вывести значения для графика функции "y = 2x - 10"
-
 int main()
 {
-    float y, k, x, b;
-    printf("Отталкиваясь от формулы kx + b = y\n");
-    printf("Введите коeффициент k:\n");
-    scanf("%g", &k);
+    int x1, x2, x3;
+    int y1, y2, y3;
     
-    printf("Введите коeффициент x:\n");
-    scanf("%g", &x);
+    printf("Введите x1: ");
+    scanf("%d", &x1);
+    printf("Введите x2: ");
+    scanf("%d", &x2);
+    printf("Введите x3: ");
+    scanf("%d", &x3);
     
-    printf("Введите коeффициент b:\n");
-    scanf("%g", &b);
-    
-    y = k * x + b ;
-    printf("Результат равен %g", y);
+    y1 = (2 * x1) - 10;
+    y2 = (2 * x2) - 10;
+    y3 = (2 * x3) - 10;
+    printf("При x = %d, y равен %d\n", x1, y1);
+    printf("При x = %d, y равен %d\n", x2, y2);
+    printf("При x = %d, y равен %d\n", x3, y3);
 }
-
