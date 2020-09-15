490ba693443260571f0fc38e49d63fd84c11aaf7
leetrikon <49814202+leetrikon@users.noreply.github.com>
2020-09-15 08:29:21 +0700

    Update Factorial.c

--- Тема 6, домашнее задание/Factorial.c
+++ Тема 6, домашнее задание/Factorial.c
@@ -1,17 +1,19 @@
-#include stdio.h
-int main() {
+#include <stdio.h>
+
+int main()
+{
     int n, i, fact;
     fact = 1;
-    printf(Введите целое число );
-    scanf(%d, &n);
-
-    if (n = 0) 
-        printf(Факториала от 0 и отрицательного числа не существует!);
+    printf("Введите целое число: ");
+    scanf("%d", &n);
+    
+    if (n <= 0)
+        printf("Факториала от 0 или отрицательного числа не существует!");
     else {
-        for (i = 1; i = n; ++i) {
-            fact = i;
-        }
+        for(i = 1; i <= n; i++)
+            fact *= i;
     }
-        printf(Факториал числа %d равен %d!, n, fact);
-    return 0;
+    printf("Факториал числа %d равен %d!", n, fact);
+    
 }
+
