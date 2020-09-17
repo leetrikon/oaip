fb623db60f55c74d10d5a9de481409037cc57a52
leetrikon <49814202+leetrikon@users.noreply.github.com>
2020-09-17 23:09:59 +0700

    Update switchcase calc.c

--- ОАИП 14.09.2020/Калькулятор/switchcase calc.c
+++ ОАИП 14.09.2020/Калькулятор/switchcase calc.c
@@ -1,1 +1,45 @@
-sdf
+#include <stdio.h>
+
+int main()
+{
+    float a, b, res;
+    char sym;
+    
+    printf("Добро пожаловать в калькулятор имени Ванина Никиты!\n");
+    printf("Введите первое число: ");
+    scanf("%f", &a);
+    printf("Введите второе число: ");
+    scanf("%f", &b);
+    printf("Что вы хотите сделать? Введите один из символов: *, /, +, -\n");
+    scanf("%s", &sym);
+    
+    switch(sym){
+        
+        case ('+'): 
+        res = a + b;
+        break;
+        
+        case ('-'): 
+        res = a - b;
+        break;
+        
+        case ('*'): 
+        res = a * b;
+        break;
+        
+        case ('/'): 
+        if(b != 0)
+            res = a / b;
+        else
+            printf("Антон Викторович, не ломайте программу!\n");
+            return 0;
+        break;
+        
+        default:
+        printf("Антон Викторович, не ломайте программу!\n");
+        return 0;
+        break;
+    }
+        
+    printf("%3.2f", res);
+}
